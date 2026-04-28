00aff0e68c57cc5a5f9182f454ce8647b20bbb17
kushalakm522 <kushalakm522@gmail.com>
2026-04-28 18:28:10 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++) {
+			for(int j=1;j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
